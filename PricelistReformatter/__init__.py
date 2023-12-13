09afccca069e5abb65dcf3bea2064115d80266b2
N0418721 <matthew.parkin2012@my.ntu.ac.uk>
2023-12-13 19:43:22 +0000

    Rmv xl file and update comments

--- PricelistReformatter/__init__.py
+++ PricelistReformatter/__init__.py
@@ -5,6 +5,8 @@ from xl_sheet_reader import *
 
 
 worksheet = xl_sheet()
+
+# Testing
 print(worksheet.product_name_column)
 print(worksheet.price_column)
 print(worksheet.category_column)
--- PricelistReformatter/xl_sheet_reader.py
+++ PricelistReformatter/xl_sheet_reader.py
@@ -2,17 +2,14 @@ from openpyxl import *
 from paths import *
 
 def get_column(column_head):   
-    # Open the spreadsheet 
-    workbook = load_workbook(source_xl_file)     
-    # Get the first sheet 
+    # Open the spreadsheet and get the first sheet
+    workbook = load_workbook(source_xl_file)
     sheet = workbook.worksheets[0]  
        
     # Create a list to store the values 
     column = []    
     # Iterate over the rows in the sheet 
     for row in sheet: 
-        # Get the value of the first cell 
-        # in the row (the "Name" cell) 
         cell = row[column_head].value 
         # Add the value to the list        
         column.append(cell)    
