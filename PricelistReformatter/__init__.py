77e5e579f696556451fbbf2d0f6dbd89fd20102f
N0418721 <matthew.parkin2012@my.ntu.ac.uk>
2023-12-14 19:12:00 +0000

    Added simple GUI
    PySimpleGUI added, need to add file upload function

--- PricelistReformatter/__init__.py
+++ PricelistReformatter/__init__.py
@@ -3,8 +3,11 @@ from paths import *
 from xl_sheet import *
 from xl_sheet_reader import *
 from xl_sheet_writer import *
+from xl_reformatter import *
 
-worksheet = xl_sheet()
+#worksheet = xl_sheet()
 
 # Testing
-write_new_workbook(worksheet)
+#write_new_workbook(worksheet)
+
+upload_xl_sheet()
---
+++ PricelistReformatter/xl_reformatter.py
@@ -0,0 +1,23 @@
+import PySimpleGUI as sg
+from openpyxl import *
+from xl_sheet_writer import *
+from xl_sheet import *
+
+def upload_xl_sheet():
+    sg.theme("DarkTeal2")
+    layout = [[sg.T("")], [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-IN-")],[sg.Button("Submit")]]    
+    window = sg.Window('My File Browser', layout, size=(600,150))
+
+    # Render window loop
+    while(True):
+        event, values = window.read()
+
+        # End App if window is closed
+        if event == sg.WIN_CLOSED or event=="Exit":
+            break
+
+        elif event == "Submit":
+            worksheet = xl_sheet()
+            write_new_workbook(worksheet)
+
+upload_xl_sheet()
